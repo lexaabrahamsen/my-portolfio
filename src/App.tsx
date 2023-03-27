1629ad4fbf7632821806f6d661f03a053dc839b9
Lexa Wong <lexa@takecommandhealth.com>
2023-03-26 23:40:48 -0400

    Add navigation, add react router , begin current work

--- src/App.tsx
+++ src/App.tsx
@@ -1,127 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import About from './Section/About';
 import Projects from './Section/Projects';
-import { useState } from 'react';
-import { Dialog } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Github } from '@styled-icons/bootstrap/Github';
-import { Linkedin } from '@styled-icons/bootstrap/Linkedin';
-import { Mastodon } from '@styled-icons/bootstrap/Mastodon';
-import { Instagram } from '@styled-icons/bootstrap/Instagram';
-
-const navigation = [
-  { name: 'About', href: '#' },
-  { name: 'Work', href: '#' },
-  { name: 'Experiments', href: '#' },
-  { name: 'Contact', href: '#' },
-  { name: 'Resume', href: '#' },
-];
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Navbar from './Navbar';
+import Homepage from './Section/Homepage';
 
 function App() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
     <>
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          className="flex items-center justify-between p-6 lg:px-8 bg-white sticky top-0"
-          aria-label="Global"
-        >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Lexa Wong</span>
-              <div className="text-black uppercase font-sans text-xl font-extrabold tracking-normal decoration-solid">
-                Lexa Wong
-              </div>
-              <div className="font-sans uppercase">
-                Frontend Developer | UI Designer
-              </div>
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6 text black" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-x-12">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="text-sm font-semibold leading-6 text-black uppercase"
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-        </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
-          <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-end">
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-white"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-white hover:line-through"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <div className="group mt-8 inline-flex flex-wrap items-center gap-3">
-                  <a href="https://github.com/lexaabrahamsen" target="_blank">
-                    <span className="cursor-pointer rounded-full bg-white border border-white-200/5 bg-white-500/5 p-3 text-white-500 transition-colors hover:border-white-500/10 hover:bg-white-500/10 hover:!opacity-100 group-hover:opacity-70">
-                      <Github className="h-5 w-5" />
-                    </span>
-                  </a>
-                  <span className="cursor-pointer rounded-full bg-white border border-white-200/5 bg-white-500/5 p-3 text-white-500 transition-colors hover:border-white-500/10 hover:bg-white-500/10 hover:!opacity-100 group-hover:opacity-70">
-                    <a href="https://www.linkedin.com/in/lexa-abrahamsen/" target="_blank">
-                      <Linkedin className="h-5 w-5" />
-                    </a>
-                  </span>
-                  <span className="cursor-pointer rounded-full bg-white border border-white-200/5 bg-white-500/5 p-3 text-white-500 transition-colors hover:border-white-500/10 hover:bg-white-500/10 hover:!opacity-100 group-hover:opacity-70">
-                    <a href="https://www.instagram.com/lexaraea/?hl=en" target="_blank">
-                      <Instagram className="h-5 w-5" />
-                    </a>
-                  </span>
-                  <span className="cursor-pointer rounded-full bg-white border border-white-200/5 bg-white-500/5 p-3 text-white-500 transition-colors hover:border-white-500/10 hover:bg-white-500/10 hover:!opacity-100 group-hover:opacity-70">
-                    <a href="" target="_blank">
-                      <Mastodon className="h-5 w-5" />
-                    </a>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
-      </header>
-      <About />
-      <Projects />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/about' element={<About />} />
+          {/* <Route path='/about' element={<Projects />} /> */}
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/Section/Homepage.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <>
+    
+    <div className="flex items-center mt-12 ml-10 mr-10">
+      <div className="bg-green-500 p-10 z-30 mt-40">
+        <h1 role="heading" className="focus:outline-none md:w-60 font-bold md:text-6xl text-4xl leading-tight text-gray-800 dark:text-white">Think out loud</h1>
+        <p role="contentinfo" className="focus:outline-none text-base leading-6 mt-8 text-gray-600 dark:text-gray-200 2xl:pr-24 xl:pr-0 max-w-lg">Don’t get carried away with super-short paragraphs. The secret to one-sentence paragraphs is to use them like a spice: A pinch of cumin can make a meal more savor.</p>
+      </div>
+      <div className="-ml-12">
+        <img src="https://i.ibb.co/wMd2Q4B/new.png" alt="Purple flowers on a book" className="md:w-full sm:w-full w-full" />
+      </div>
+    </div>
+
+    {/* <div className="sm:flex items-center justify-between w-full bg-orange-500">
+        <div className="xl:w-1/3 md:w-1/2 w-full justify-items-center bg-purple-400 pl-20 pr-20 ml-20">
+            <h1 role="heading" className="focus:outline-none md:w-60 font-bold md:text-6xl text-4xl leading-tight text-gray-800 dark:text-white">Think out loud</h1>
+            <p role="contentinfo" className="focus:outline-none text-base leading-6 mt-8 text-gray-600 dark:text-gray-200 2xl:pr-24 xl:pr-0">Don’t get carried away with super-short paragraphs. The secret to one-sentence paragraphs is to use them like a spice: A pinch of cumin can make a meal more savor.</p>
+        </div>
+        <div className="xl:w-4/6 md:w-1/2 w-full xl:pl-48 bg-pink-500">
+          <div className="w-full">
+              <img src="https://i.ibb.co/wMd2Q4B/new.png" alt="Purple flowers on a book" className="md:w-full sm:w-1/2 w-full" />
+          </div>
+        </div>
+    </div>
+
+
+    <div className="xl:w-4/6 md:w-1/2 w-full xl:pl-48">
+        <p role="contentinfo" className="focus:outline-none text-base leading-6 mb-8 text-gray-600 dark:text-gray-200 md:mt-0 mt-10">“Don’t get carried away with super-short paragraphs. The secret to one-sentence paragraphs is to use them like a spice: A pinch of cumin can make a meal more savory, but I don’t want to eat a plateful of the stuff.”</p>
+        <div className="w-full">
+            <img src="https://i.ibb.co/wMd2Q4B/new.png" alt="Purple flowers on a book" className="md:w-full sm:w-1/2 w-full" />
+            <div className="flex items-center mt-8">
+                <p role="contentinfo" className="text-xl focus:outline-none font-semibold leading-5 text-gray-700 dark:text-white">Picture description</p>
+                <p role="contentinfo" className="text-xl focus:outline-none italic font-italic leading-5 pl-2 text-gray-700 dark:text-white">Specification</p>
+            </div>
+        </div>
+    </div>
+    <div className="grid grid-cols-1 bg-orange-500 px-10">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          Hi, I'm Reed.
+          <br className="hidden lg:inline-block" />I love to build amazing
+          apps.
+        </h1>
+        <p className="mb-8 leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+          laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+          Laborum, voluptas natus?
+        </p>
+    </div> */}
+{/*     
+    <div className="flex bg-orange-300 px-10">
+      <p className="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+        Laborum, voluptas natus?
+      </p>
+      <p className="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+        Laborum, voluptas natus?
+      </p>
+      <p className="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+        Laborum, voluptas natus?
+      </p>
+    </div> */}
+    {/* <section id="about-change-classname">
+    <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center bg-orange-500">
+      <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center bg-blue-500">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          Hi, I'm Reed.
+          <br className="hidden lg:inline-block" />I love to build amazing
+          apps.
+        </h1>
+        <p className="mb-8 leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+          laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+          Laborum, voluptas natus?
+        </p>
+        <div className="flex justify-center">
+          <a
+            href="#contact"
+            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+            Work With Me
+          </a>
+          <a
+            href="#projects"
+            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+            See My Past Work
+          </a>
+        </div>
+      </div>
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 bg-blue-500">
+        <img
+          className="object-cover object-center rounded"
+          alt="hero"
+          src="./coding.svg"
+        />
+      </div>
+    </div>
+  </section> */}
+  </>
+
+  );
+}
